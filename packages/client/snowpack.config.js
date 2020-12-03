a34fc24f3553261b06eccebae7ac1ae77907f3d1
Ben Cates <ben@mojotech.com>
2020-12-03 15:48:30 -0700

    Run the server and client together
    Sets Snowpack to run the server as a child process and proxies the
    `/graphql` endpoint to it.

--- packages/client/snowpack.config.js
+++ packages/client/snowpack.config.js
@@ -5,26 +5,26 @@ module.exports = {
     src: '/_dist_',
   },
   plugins: [
+    [
+      '@snowpack/plugin-run-script',
+      {
+        name: 'Server Console',
+        // We only want to run the server in development
+        cmd: '',
+        watch: 'cd ../server && yarn start',
+      },
+    ],
     '@snowpack/plugin-dotenv',
     '@snowpack/plugin-typescript',
     '@prefresh/snowpack',
   ],
-  install: [
-    /* ... */
-  ],
   installOptions: {
     installTypes: true,
   },
-  devOptions: {
-    /* ... */
-  },
-  buildOptions: {
-    /* ... */
-  },
   proxy: {
-    /* ... */
+    '/graphql': {
+      target: 'http://localhost:3000',
+      changeOrigin: true,
+    },
   },
-  alias: {
-    /* ... */
-  },
-};
+}
