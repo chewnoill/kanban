45cbdc705e36c507e03f55e76370537c5d51b611
Ben Cates <ben@mojotech.com>
2020-12-03 14:48:39 -0700

    Set a project-wide prettier config
    In my opinion `"trailingComma": "all"` justifies the creation of the
    file. Turning it on in a greenfield project reduces noise in the git
    logs.
    
    The other two settings are personal preference.

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,16 +1,16 @@
-import { h } from 'preact';
-import { useState, useEffect } from 'preact/hooks';
-import logo from './logo.png';
-import './App.css';
+import { h } from 'preact'
+import { useState, useEffect } from 'preact/hooks'
+import logo from './logo.png'
+import './App.css'
 
 function App() {
   // Create the count state.
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0)
   // Create the counter (+1 every second).
   useEffect(() => {
-    const timer = setTimeout(() => setCount(count + 1), 1000);
-    return () => clearTimeout(timer);
-  }, [count, setCount]);
+    const timer = setTimeout(() => setCount(count + 1), 1000)
+    return () => clearTimeout(timer)
+  }, [count, setCount])
   // Return the App component.
   return (
     <div className="App">
@@ -34,7 +34,7 @@ function App() {
         </p>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -1,8 +1,8 @@
-import { makeExecutableSchema } from "graphql-tools";
-import { GraphQLModule } from "@graphql-modules/core";
-import { graphqlHTTP } from "express-graphql";
-import gql from "graphql-tag";
-const express = require("express");
+import { makeExecutableSchema } from 'graphql-tools'
+import { GraphQLModule } from '@graphql-modules/core'
+import { graphqlHTTP } from 'express-graphql'
+import gql from 'graphql-tag'
+const express = require('express')
 
 const App = new GraphQLModule({
   typeDefs: [
@@ -10,20 +10,20 @@ const App = new GraphQLModule({
       type Query {
         status: String!
       }
-    `
+    `,
   ],
   resolvers: {
     Query: {
-      status: () => "Hello World"
-    }
-  }
-});
+      status: () => 'Hello World',
+    },
+  },
+})
 
-const schema = makeExecutableSchema(App.forRoot({}));
+const schema = makeExecutableSchema(App.forRoot({}))
 
-const PORT = 3000;
+const PORT = 3000
 
-const app = express();
-app.use(graphqlHTTP({ schema, graphiql: true }));
+const app = express()
+app.use(graphqlHTTP({ schema, graphiql: true }))
 
-app.listen(PORT);
+app.listen(PORT)
